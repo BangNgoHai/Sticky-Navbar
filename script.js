9dba2c46c9d0d3b9ed4d5505cea951e7aea4cae2
HaiBangNgo <sonbangmtp@gmail.com>
2023-01-14 10:36:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const navbarEl = document.querySelector('.navbar');
+
+const bottomContainerEl = document.querySelector('.bottom-container');
+
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > bottomContainerEl.offsetTop - navbarEl.offseHeight -50){
+        navbarEl.classList.add("active");
+    }else{
+        navbarEl.classList.remove("active");
+    }
+});
